b1520db2cdf06f71ab81121b3d9d2bec4bd07d59
George <gwwebster23@gmail.com>
2023-05-02 20:12:17 -0500

    React state lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [liClass, setLiClass] = useState("");
+
+  const buttonText = liClass === "" ? "Add to Cart" : "Remove From Cart";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => liClass === "" ? setLiClass("in-cart") : setLiClass("")}>
+        {buttonText}
+      </button>
     </li>
   );
 }
